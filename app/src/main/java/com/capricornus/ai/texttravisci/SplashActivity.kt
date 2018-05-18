7989144af34fd74cc988f6ddc30afdb7d5d4f298
Capricornus <739388842@qq.com>
2018-05-18 15:26:43 +0800

    删除name2

--- app/src/main/java/com/capricornus/ai/texttravisci/SplashActivity.kt
+++ app/src/main/java/com/capricornus/ai/texttravisci/SplashActivity.kt
@@ -24,7 +24,6 @@ class SplashActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         }
         var name="1"
         var names='2'
-        var name2=' '
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
